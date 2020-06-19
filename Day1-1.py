6bbf1471f8bb91bf6dbfe2925f963c54d95db5c8
ajaydr16 <55269615+ajaydr16@users.noreply.github.com>
2020-06-19 17:00:06 +0530

    Update Day1-1.py

--- Day1-1.py
+++ Day1-1.py
@@ -1,14 +1,12 @@
-# your code goes here
-l=[]
-p=int(input())
-for i in range(p):
-    l1=list(map(str,input().split()))
-    l.append(l1)
-for i in range(p):
-    m=l[i][0]
-    n=l[i][1]
-    m=int(m[-1::-1])
-    n=int(n[-1::-1])
-    r=str(m+n)
-    r=int(r[-1::-1])
-    print(r)
+#Day1-1
+for i in range(int(input())):
+    m,n=input().split()
+    m,n=int(m),int(n)
+    l={}
+    for j in range(2,int(n**0.5)+1):
+        for k in range(max(m//j,2),(n//j)+1):
+            l[j*k]=1
+    for j in range(max(m,2),n+1):
+        if j not in l:
+            print(j)
+    print()
